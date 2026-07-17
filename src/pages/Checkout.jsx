0f1803ee5057dc3f634b2be9b913c350c2dcf070
KMCR41207 <qwertyasd077a@gmail.com>
2026-07-17 23:32:45 +0530

    feat: replace UPI app logos with real professional logos from CDN

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -271,31 +271,32 @@ const openUpiApp = (appId, upiId, amount) => {
 
 const UPI_LOGOS = {
   gpay: (
-    <svg width="28" height="28" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg">
-      <path fill="#4285F4" d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"/>
-      <path fill="#34A853" d="M24 48c6.48 0 11.93-2.13 15.89-5.81l-7.73-6c-2.15 1.45-4.92 2.3-8.16 2.3-6.26 0-11.57-4.22-13.47-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48z"/>
-      <path fill="#FBBC05" d="M10.53 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.97-6.19z"/>
-      <path fill="#EA4335" d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"/>
-      <path fill="#4285F4" d="M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z"/>
-    </svg>
+    <img 
+      src="https://cdn.worldvectorlogo.com/logos/google-pay-2.svg" 
+      alt="Google Pay" 
+      style={{ width: '28px', height: '28px', objectFit: 'contain' }}
+    />
   ),
   phonepe: (
-    <svg width="28" height="28" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg">
-      <circle cx="24" cy="24" r="24" fill="#5F259F"/>
-      <path fill="#fff" d="M30.5 13h-7.1L14 25.5V33h5v-5.5l3.5-4.5H26c3.6 0 6.5-2.9 6.5-6.5S34.1 10 30.5 10v3zm0 10H23l4-5h3.5c1.9 0 3.5 1.6 3.5 3.5S32.4 23 30.5 23z"/>
-    </svg>
+    <img 
+      src="https://cdn.worldvectorlogo.com/logos/phonepe-4.svg" 
+      alt="PhonePe" 
+      style={{ width: '28px', height: '28px', objectFit: 'contain' }}
+    />
   ),
   paytm: (
-    <svg width="28" height="28" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg">
-      <rect width="48" height="48" rx="8" fill="#00B9F1"/>
-      <path fill="#fff" d="M8 30h4v-8h4v-4H8zm12-12h4v12h-4zm6 0h4v4h4v8h-4v-4h-4zm10 0h4v12h-4z"/>
-    </svg>
+    <img 
+      src="https://cdn.worldvectorlogo.com/logos/paytm.svg" 
+      alt="Paytm" 
+      style={{ width: '28px', height: '28px', objectFit: 'contain' }}
+    />
   ),
   bhim: (
-    <svg width="28" height="28" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg">
-      <rect width="48" height="48" rx="8" fill="#00539B"/>
-      <text x="50%" y="58%" dominantBaseline="middle" textAnchor="middle" fill="#fff" fontSize="14" fontWeight="bold" fontFamily="Arial">BHIM</text>
-    </svg>
+    <img 
+      src="https://cdn.worldvectorlogo.com/logos/bhim-2.svg" 
+      alt="BHIM" 
+      style={{ width: '28px', height: '28px', objectFit: 'contain' }}
+    />
   ),
 };
 
